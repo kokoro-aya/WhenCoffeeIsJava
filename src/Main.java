0e32731a97ad28816316385aa2b6a767345c96ed
kokoro-aya <35118912+kokoro-aya@users.noreply.github.com>
2024-11-11 21:31:24 +0100

    Implemented a simple `when` expression
    - Limitation: no return value, only `void` type
    - Remark: it's impossible to have a fully type-safe implementation since it's impossible to make covariant functional interface type in Java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+import nat.Nat;
+import nat.O;
+import nat.S;
+import shapes.Circle;
+import shapes.Rectangle;
+import shapes.Shape;
+import shapes.Square;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new When<Nat>(new S(new S(new O())))
+                .is(S.class, (S s) -> {
+                  System.out.println(s.prev());
+                })
+                .is(O.class, (O o) -> {
+                  System.out.println(o.next());
+                })
+                .execute();
+
+
+        new When<Nat>(new O())
+                .is(S.class, (S s) -> {
+                  System.out.println("S(?)");
+                })
+                .is(O.class, (O o) -> {
+                  System.out.println("? O");
+                })
+                .execute();
+
+        new When<Shape>(new Rectangle())
+                .is(Circle.class, (Circle circle) -> {
+                  System.out.println("Circle");
+                })
+                .is(Square.class, (Square square) -> {
+                  System.out.println("Square");
+                })
+                .otherwise((Shape shape) -> {
+                  System.out.println("Shape");
+                })
+                .execute();
     }
 }
--- src/When.java
+++ src/When.java
@@ -1,19 +1,38 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Consumer;
 
 public class When<T> {
+
+    private final Map<Class<? extends T>, Consumer<? extends T>> arms = new HashMap<>();
+
+    private Consumer<T> defaultArm = null;
+
+    private final T valueToMatch;
+
     public When(T object) {
+        this.valueToMatch = object;
     }
 
-    public <U extends T> When<T> is(Class<U> clazz, Consumer<U> fn) {
+    public <U extends T> When<T> is(Class<U> clazz, Consumer<? extends U> fn) {
+        this.arms.put(clazz, fn);
         return this;
     }
 
     public When<T> otherwise(Consumer<T> fn) {
+        this.defaultArm = fn;
         return this;
     }
 
     public void execute() {
-
+        for (Class<? extends T> clazz : this.arms.keySet()) {
+            Consumer<T> fn = (Consumer<T>) this.arms.get(clazz);
+            if (clazz.isInstance(this.valueToMatch)) {
+                fn.accept(this.valueToMatch);
+                return;
+            }
+        }
+        this.defaultArm.accept(this.valueToMatch);
     }
 
 }
