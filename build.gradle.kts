d59819067a27f7eb8f43c31f523c1ff6fda4c40f
kokoro-aya <35118912+kokoro-aya@users.noreply.github.com>
2024-11-18 00:41:19 +0100

    Update gradle script for Java target version and publishing script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,10 @@
 plugins {
     id("java")
+    id("maven-publish")
+}
+
+java {
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 group = "moe.irony.java.when"
@@ -16,4 +21,37 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            groupId = "moe.irony.java.when"
+            artifactId = "when-coffee-is-java"
+            version = "1.0-SNAPSHOT"
+
+            from(components["java"])
+
+            pom {
+                name = "When the Coffee is Java"
+                description = "A type-safe Java 8 library to simulate the Kotlin \"when\" construct"
+                url = "https://github.com/kokoro-aya/WhenCoffeeIsJava"
+                properties = mapOf(
+                )
+                licenses {
+                    license {
+                        name = "BSD-3-Clause license"
+                        url = "https://github.com/kokoro-aya/WhenCoffeeIsJava/blob/main/LICENSE.md"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "kokoro-aya"
+                        name = "irony"
+                    }
+                }
+            }
+
+        }
+    }
 }
