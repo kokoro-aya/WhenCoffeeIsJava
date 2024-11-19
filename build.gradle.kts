4b1f848b0a661ebabd8c1d0e34eb7bd50b18dec4
kokoro-aya <35118912+kokoro-aya@users.noreply.github.com>
2024-11-19 23:53:55 +0100

    Update build.gradle.kts and release.yml to publish into central portal of maven registry

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,18 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     id("java")
-    id("maven-publish")
+    id("com.vanniktech.maven.publish") version ("0.30.0")
 }
 
 java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 group = "moe.irony.java.when"
-version = "0.9-SNAPSHOT"
+version = "0.9.0"
+
 
 repositories {
     mavenCentral()
@@ -23,15 +27,34 @@ tasks.test {
     useJUnitPlatform()
 }
 
-publishing {
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/kokoro-aya/WhenCoffeeIsJava")
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+
+    coordinates("moe.irony.java.when", "when-coffee-is-java", "0.9.0")
+    pom {
+        name.set("When the Coffee is Java")
+        description.set("A type-safe Java 8 library to simulate the Kotlin \"when\" construct")
+        inceptionYear.set("2024")
+        url.set("https://github.com/kokoro-aya/WhenCoffeeIsJava/")
+        licenses {
+            license {
+                name.set("BSD-3-Clause license")
+                url.set("https://opensource.org/license/bsd-3-clause/")
+                distribution.set("https://github.com/kokoro-aya/WhenCoffeeIsJava/blob/main/LICENSE.md")
             }
+        }
+        developers {
+            developer {
+                id.set("kokoro-aya")
+                name.set("irony")
+                url.set("https://github.com/kokoro-aya")
+            }
+        }
+        scm {
+            url.set("https://github.com/kokoro-aya/WhenCoffeeIsJava/")
+            connection.set("scm:git:git://github.com/kokoro-aya/WhenCoffeeIsJava.git")
+            developerConnection.set("scm:git:ssh://git@github.com/kokoro-aya/WhenCoffeeIsJava.git")
         }
     }
 }
