e6f37f57e23253469ca557cdb707fac6776d7166
kokoro-aya <35118912+kokoro-aya@users.noreply.github.com>
2024-11-27 20:35:45 +0100

    Remove `RawPattern` class which is no longer in use

--- src/main/java/moe/irony/java/when/base/PatternVisitor.java
+++ src/main/java/moe/irony/java/when/base/PatternVisitor.java
@@ -11,7 +11,6 @@ public interface PatternVisitor<T, R> {
   ChainedResult<R> visit(NullPattern<T, R> pattern);
   ChainedResult<R> visit(OtherwisePattern<T, R> pattern);
   ChainedResult<R> visit(RangePattern<T, R> pattern);
-  ChainedResult<R> visit(RawPattern<T, R> pattern);
 
   ChainedResult<R> visit(MultiArmPattern<T, R> pattern);
 }
